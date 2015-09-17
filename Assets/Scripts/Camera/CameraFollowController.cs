f44c2e02f830103c03ff2f38142e28ccec03f00b
Alex Schaeffer <AMVPh34r@gmail.com>
2015-09-17 11:26:15 -0400

    Implement basic isometric player movement

---
+++ Assets/Scripts/Camera/CameraFollowController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowController : MonoBehaviour {
+    public Transform target;
+    public float speed = 5f;    // Camera follow speed
+
+    private Vector3 offset;         // Initial camera offset
+
+
+    void Start() {
+        this.offset = transform.position - target.position;
+    }
+
+
+    void FixedUpdate() {
+        Vector3 camPos = target.position + this.offset;
+        transform.position = Vector3.Lerp(transform.position, camPos, speed * Time.deltaTime);
+    }
+}
