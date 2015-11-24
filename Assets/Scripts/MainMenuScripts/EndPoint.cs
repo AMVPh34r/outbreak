fc391aeae2a7391e3f41a6642d78b13cebffbbb5
brannon sibounheuang <bsibounh@uncc.edu>
2015-11-24 15:09:22 -0500

    EndPoint for End game and text

---
+++ Assets/Scripts/MainMenuScripts/EndPoint.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndPoint : MonoBehaviour {
+
+
+	public int sceneToStart = 0;				//Index number in build settings of scene to load if changeScenes is true
+
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		// If the colliding gameobject is the player...
+		if(other.gameObject == player)
+		{
+			// ... play the clip at the position of the key...
+			AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+			
+
+			
+			LoadDelayed();
+		}
+	}
+
+
+
+	public void LoadDelayed()
+	{
+		
+		//Load the selected scene, by scene index number in build settings
+		Application.LoadLevel (sceneToStart);
+	}
+
+}
+
+
+
