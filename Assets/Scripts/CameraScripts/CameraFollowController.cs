198c9bf964b1e179553db92883c0eb06e2cc2602
Alex Schaeffer <AMVPh34r@gmail.com>
2015-12-08 21:34:59 -0500

    Implement very basic scroll wheel zooming
    TODO: Make zooming smoother, use position/rotation rather than FOV

--- Assets/Scripts/CameraScripts/CameraFollowController.cs
+++ Assets/Scripts/CameraScripts/CameraFollowController.cs
@@ -6,6 +6,10 @@ public class CameraFollowController : MonoBehaviour {
     public float speed = 5f;    	// Camera follow speed
 
     private Vector3 offset;         // Initial camera offset
+	private float fov;
+	private float minFov = 15f;
+	private float maxFov = 90f;
+	private float sensitivity = 1f;
 
 
     void Start() {
@@ -16,5 +20,12 @@ public class CameraFollowController : MonoBehaviour {
     void FixedUpdate() {
         Vector3 camPos = target.position + this.offset;
         transform.position = Vector3.Lerp(transform.position, camPos, speed * Time.deltaTime);
+
+		// Zoom camera when scroll wheel is spun
+		fov = Camera.main.fieldOfView;
+		Debug.Log (Input.GetAxis ("Mouse ScrollWheel"));
+		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+		fov = Mathf.Clamp(fov, minFov, maxFov);
+		Camera.main.fieldOfView = fov;
     }
 }
